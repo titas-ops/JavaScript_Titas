4bf3060060bb73721a68988436085d9629ef9fad
Titas Dey <devemailoftitas@gmail.com>
2025-03-06 13:38:10 +0530

    variables done

--- 01_Basics/variable.js
+++ 01_Basics/variable.js
@@ -1,3 +1,7 @@
 let name = 'Titas'
 
 console.log(name);
+
+// const --> can not change value after first assignment
+//let --> value can be changed later
+// var : do not use , it has scoping issues
