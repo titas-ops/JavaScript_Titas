7ef3961883d6d5b46eb1d4d8231246b995478d16
Titas Dey <devemailoftitas@gmail.com>
2025-03-06 13:44:31 +0530

    datatype done

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,24 @@
+
+
+console.log(3+3) 
+console.log('Titas')
+
+
+let name = "Titas"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
