2efecad186af6802c31fed526fc1abc4b54e1cb0
Titas Dey <devemailoftitas@gmail.com>
2025-03-10 22:00:11 +0530

    arrays 7 problems done , march 11 ends

--- 01_Basics/06_someMoreArray.js
+++ 01_Basics/06_someMoreArray.js
@@ -41,4 +41,39 @@ console.log(isSorted([1, 2, 3, 4, 5])); // true
 console.log(isSorted([1, 3, 2, 4, 5])); // false
 
 
+function rotateArray(arr, k) {
+    k = k % arr.length;
+    return [...arr.slice(-k), ...arr.slice(0, arr.length - k)];
+}
+
+// Example usage
+console.log(rotateArray([1, 2, 3, 4, 5], 2)); // [4, 5, 1, 2, 3]
+
+
+function findMissingNumber(arr, n) {
+    let total = (n * (n + 1)) / 2;
+    let sum = arr.reduce((acc, num) => acc + num, 0);
+    return total - sum;
+}
+
+// Example usage
+console.log(findMissingNumber([1, 2, 4, 5, 6], 6)); // 3
+
+
+function secondLargest(arr) {
+    let first = -Infinity, second = -Infinity;
+    for (let num of arr) {
+        if (num > first) {
+            second = first;
+            first = num;
+        } else if (num > second && num !== first) {
+            second = num;
+        }
+    }
+    return second === -Infinity ? null : second;
+}
+
+// Example usage
+console.log(secondLargest([10, 20, 4, 45, 99])); // 45
+
 
